31d53719e8c4d9ea78a594ea62e776f65d519690
Lasse <1869424+lasseeee@users.noreply.github.com>
2020-07-07 23:27:18 +0200

    Move routes to facade

--- src/resources/views/nb/faq.blade.php
+++ src/resources/views/nb/faq.blade.php
@@ -1,13 +1,13 @@
 @component('info.faq')
 ## Hvorfor må jeg godta avtalevilkårene og personvernerklæringen?
 
-For at {{ $currentTenant->name }} Røde Kors skal kunne oppfylle avtalevilkårene er vi nødt til å lagre noen personopplysninger om deg, som nevnt i personvernerklæringen.
+For at {{ optional($currentTenant)->name }} Røde Kors skal kunne oppfylle avtalevilkårene er vi nødt til å lagre noen personopplysninger om deg, som nevnt i personvernerklæringen.
 
 ## Hva om jeg ikke ønsker å godta?
 
-Om du ikke ønsker å godta avtalevilkårene og personvernerklæringen kan du selv stoppe behandlingen av dine data ved å melde deg ut av aktivitene du er aktiv i, eller ta kontakt med {{ $currentTenant->name }} Rød Kors med ønske om dette.
+Om du ikke ønsker å godta avtalevilkårene og personvernerklæringen kan du selv stoppe behandlingen av dine data ved å melde deg ut av aktivitene du er aktiv i, eller ta kontakt med {{ optional($currentTenant)->name }} Rød Kors med ønske om dette.
 
 ## Hva om jeg har enda et spørsmål?
 
-Ta kontakt med {{ $currentTenant->name }} Røde Kors på epost {{ $currentTenant->email }}, telefon {{ $currentTenant->phone_number }}, eller kom innom {{ $currentTenant->name }} Røde Kors, {{ $currentTenant->address }}.
+Ta kontakt med {{ optional($currentTenant)->name }} Røde Kors på epost {{ optional($currentTenant)->email }}, telefon {{ optional($currentTenant)->phone_number }}, eller kom innom {{ optional($currentTenant)->name }} Røde Kors, {{ optional($currentTenant)->address }}.
 @endcomponent
