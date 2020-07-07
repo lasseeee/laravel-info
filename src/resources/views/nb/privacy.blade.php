fa7c95b4998608606e0c14db0cfe41907814567a
Lasse <1869424+lasseeee@users.noreply.github.com>
2020-07-07 09:43:50 +0200

    Inital commit

---
+++ src/resources/views/nb/privacy.blade.php
@@ -0,0 +1,72 @@
+@component('info.privacy')
+## Hvem er behandlingsansvarlig?
+
+{{ $currentTenant->name }} Røde Kors, ved daglig leder, er behandlingsansvarlig, og vil behandle og være ansvarlig for personopplysningene som lagres om deg i dette datasystemet for koordinering av {{ config('app.name') }}.
+
+## Hva er formålet med behandlingen?
+
+Formålet med behandlingen er å tilrettelegge for trygge, effektive og frivillige aktiviteter i {{ $currentTenant->name }} Røde Kors. Dette gjøres ved å benytte dette datasystemet til å koordinere denne aktiviteten, avdekke avvik, ivareta sikkerhet, og utarbeide anonymisert statistikk for å søke om driftsmidler.
+
+## Hvilke personopplysninger behandles?
+
+For å oppnå formålet er det nødvendig å behandle et minimum av personopplysninger om deg. Personopplysninger utover disse er det valgfritt å oppgi, og er markert med teksten "valgfritt" i grensesnittet.
+
+### Personopplysninger du oppgir
+
+#### Som deltaker
+
+**Navn**, **fødselsår**, **bostedområde**, og **kjønn** for å kunne identifisere deg som en unik deltaker for å kunne hente ut anonymisert statistikk til å søke om driftsmidler, følge opp aktiviteten, og for å ivareta din, og andres, sikkerhet i tilfelle det skulle oppstå brann eller liknende.
+
+#### Som frivillig eller ansatt
+
+**Navn** for at andre frivillige og ansatte i {{ $currentTenant->name }} Røde Kors skal vite hvem du er; **epostadresse** som er din innloggingsbruker, og datasystemets hovedkommunikasjonskanal med deg ved relevante hendelser, som for eksempel beskjed ved nye logger og kommentarer; **foretrukket språk av norsk og engelsk** for å vise grensesnittet på best egnet språk, **logger** og **kommentarer** du skriver for at aktiviteten skal kunne følges opp på effektiv måte, og for å avdekke avvik; **begrunnelse for utestengelse**, **utestengt aktivitet** og **utestengt senter** for at deltakerne skal vite hvorfor de er utestengt, og for at du skal kunne ivareta sikkerheten.
+
+#### Som administrator
+**Rolletilgang** for brukere i systemet for å gi brukere tilgang til kun relevant informasjon.
+
+### Personopplysninger datasystemet lagrer
+
+#### Som deltaker
+
+**Besøkstidspunkt**, **besøkt aktivitet**, og **besøkt senter** lagres når du sjekker inn for å kunne hente ut anonymisert statistikk, for å følge opp aktiviteten, for å søke om midler for videre drift, og for å ivareta din, og andres, sikkerhet; **begrunnelse for utestengelse**, **utestengt aktivitet** og **utestengt senter** lagres for at du skal vite hvorfor du er utestengt, og for at de frivillige og ansatte ved {{ $currentTenant->name }} Røde Kors skal kunne ivareta sikkerheten.
+
+#### Som frivillig eller ansatt
+
+**Tidspunkt for siste innlogging** for å vise deg hva som er nytt siden sist du logget inn; **IP-adressen** til enheten du benytter til å logge inn med, og **endringer du gjør** for å ivareta informasjonssikkerheten, og for å oppdage og rapportere eventuelle avvik.
+
+### Personopplysninger fra tredjepart
+
+Det lagres ingen personopplysninger fra tredjepart i dette datadatasystemet.
+
+## Hva er grunnlaget for behandlingen?
+
+Grunnlaget for behandlingen er basert på en interesseavveing, hvor det ideelle arbeidet {{ $currentTenant->name }} Røde Kors utfører kommer den registrerte til gode, uten at dette utgjør et unødvendig inngrep i personvernet.
+
+For enhver behandling utover dette er grunnlaget ditt samtykke. Du må ha fylt 15 år for å samtykke. Om du ikke har fylt 15 år kan dine foreldre samtykke for deg. Samtykket er gyldig til du selv trekker dette tilbake. Du kan når som helst trekke samtykket tilbake. Du kan trekke samtykket tilbake fra din brukerprofil, eller ved å ta kontakt med {{ $currentTenant->name }} Røde Kors. Å trekke tilbake samtykket forhindrer deg ikke fra å delta i denne aktiviteten, og har ingen negative konsekvenser for deg.
+
+## Hvem utleveres personopplysningene til?
+
+{{ $currentTenant->name }} Røde Kors har skrevet databehandleravtale, i tråd med EU-forordningens krav, med databehandler innenfor EU/EØS for sikker behandling av dine personopplysninger og varsling ved avvik. Dine opplysninger utleveres ikke til annen tredjepart uten ditt samtykke.
+
+## Hvor lenge lagres personopplysningene?
+
+Personopplysningene lagres i inntil 2 år etter siste innsjekk som deltaker, og i inntil 2 år etter siste innlogging som frivillig eller ansatt. Andre personopplysninger lagres i inntil 2 år etter at de ble oppgitt. Du kan når som helst be om få dine personopplysninger slettet.
+
+## Hva er min rettigheter?
+
+Du har rett til å få innsyn i alle personopplysninger som er lagret om deg; få utlevert opplysningner slik at du kan ta disse med videre til en annen organisasjon (rett til dataportabilitet); få korrigert dine personopplysninger om disse skulle være feilaktige (krav om riktighet); kreve at behandlingen av personopplysninger begrenses; motsette deg visse former for behandling; samt få slettet alle personopplysninger som er lagret om deg.
+
+Du benytter deg av dine rettigheter ved å logge inn på din brukerprofil, eller ved å ta kontakt med {{ $currentTenant->name }} Røde Kors.
+
+## Foretas det automatiske avgjørelser?
+
+Det foretas ikke automatiske avgjørelser, eller profilering, basert på dine personopplysninger.
+
+## Hva er min klagerett?
+
+Du har rett til å klage til {{ $currentTenant->name }} Røde Kors ved behandling av personopplysninger som strider med reglene for personvern. Om dette ikke leder frem, kan du ta saken videre til Datatilsynet.
+
+## Hvordan tar jeg kontakt?
+
+For henvendelser angående personvern, ta kontakt med {{ $currentTenant->name}} Røde Kors direkte, på epost {{ $currentTenant->email }}, eller på telefon {{ $currentTenant->phone_number }}.
+@endcomponent
