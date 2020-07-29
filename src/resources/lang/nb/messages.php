97cf6ffbd6365f636b8352fdbd0f350c901c6477
Lasse <1869424+lasseeee@users.noreply.github.com>
2020-07-29 18:40:52 +0200

    Correctly add translations

--- src/resources/lang/nb/messages.php
+++ src/resources/lang/nb/messages.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-    'understand' => 'Takk for at du forstår våre bruksvilkår og vår personvernserklæring.',
+    'understood' => 'Takk for at du forstår våre bruksvilkår og vår personvernerklæring.',
+    'agree' => 'Ved fortsatt bruk av dette systemet, er du enig i disse.',
+    'and' => 'og vår',
+    'more' => 'Lær mer',
+    'privacy' => 'personvernerklæring'
+    'terms' => 'bruksvilkår',
+    'understand' => 'Jeg forstår',
+    'updated' => 'Vi har oppdater våre',
 ];
