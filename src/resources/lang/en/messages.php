a332b1db1d10c6720ab233dec4fe64afffaa6759
Lasse <post@lasseee.com>
2020-07-29 18:46:14 +0200

    Add missing comma

--- src/resources/lang/en/messages.php
+++ src/resources/lang/en/messages.php
@@ -4,7 +4,7 @@ return [
     'agree' => 'By continued use of this system, you agree to these.',
     'and' => 'and',
     'more' => 'Learn more',
-    'privacy' => 'privacy policy'
+    'privacy' => 'privacy policy',
     'terms' => 'terms of service',
     'understand' => 'I understand',
     'understood' => 'Thank you for understanding our terms of service and privacy policy.',
