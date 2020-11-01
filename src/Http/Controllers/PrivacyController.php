cd17ccbb9e1fa9228fe926cf556ebe70e2f36ef8
Lasse <1869424+lasseeee@users.noreply.github.com>
2020-11-01 19:53:32 +0100

    Fix user land views bug

--- src/Http/Controllers/PrivacyController.php
+++ src/Http/Controllers/PrivacyController.php
@@ -6,8 +6,8 @@ class PrivacyController extends Controller
 {
     public function __invoke()
     {
-        return view()->exists('info::' . config('app.locale') . '.privacy') ?
-        view('info::' . config('app.locale') . '.privacy') :
+        return view()->exists('info.' . config('app.locale') . '.privacy') ?
+        view('info.' . config('app.locale') . '.privacy') :
         view('info::nb.privacy');
     }
 }
