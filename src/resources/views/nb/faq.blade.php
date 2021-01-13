1aa5729b466998f0f21665da9e0bdfe79415e5cb
Lasse <post@lasseee.com>
2021-01-13 12:15:32 +0100

    Remove tenant dependency from code

--- src/resources/views/nb/faq.blade.php
+++ src/resources/views/nb/faq.blade.php
@@ -1,13 +1,13 @@
 @component('info::faq')
 ## Hvorfor må jeg godta avtalevilkårene og personvernerklæringen?
 
-For at {{ optional($currentTenant)->name }} Røde Kors skal kunne oppfylle avtalevilkårene er vi nødt til å lagre noen personopplysninger om deg, som nevnt i personvernerklæringen.
+For at din lokaleavdelding i Røde Kors skal kunne oppfylle avtalevilkårene er vi nødt til å lagre noen personopplysninger om deg, som nevnt i personvernerklæringen.
 
 ## Hva om jeg ikke ønsker å godta?
 
-Om du ikke ønsker å godta avtalevilkårene og personvernerklæringen kan du selv stoppe behandlingen av dine data ved å melde deg ut av aktivitene du er aktiv i, eller ta kontakt med {{ optional($currentTenant)->name }} Rød Kors med ønske om dette.
+Om du ikke ønsker å godta avtalevilkårene og personvernerklæringen kan du selv stoppe behandlingen av dine data ved å melde deg ut av aktivitene du er aktiv i, eller ta kontakt med din lokalavdeling i Røde Kors med ønske om dette.
 
 ## Hva om jeg har enda et spørsmål?
 
-Ta kontakt med {{ optional($currentTenant)->name }} Røde Kors på epost {{ optional($currentTenant)->email }}, telefon {{ optional($currentTenant)->phone_number }}, eller kom innom {{ optional($currentTenant)->name }} Røde Kors, {{ optional($currentTenant)->address }}.
+Ta kontakt med din lokalavdeling i Røde Kors på e-post, telefon  eller kom innom din lokalavdeling i Røde Kors.
 @endcomponent
