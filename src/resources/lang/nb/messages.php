ffff526667a8b2dc468feae9b3c68a214691543e
Lasse <1869424+lasseeee@users.noreply.github.com>
2021-03-04 08:07:54 +0100

    Remove terms of service

--- src/resources/lang/nb/messages.php
+++ src/resources/lang/nb/messages.php
@@ -1,12 +1,9 @@
 <?php
 
 return [
-    'understood' => 'Takk for at du forstår våre bruksvilkår og vår personvernerklæring.',
-    'agree' => 'Ved fortsatt bruk av dette systemet, er du enig i disse.',
-    'and' => 'og vår',
-    'more' => 'Lær mer',
+    'understood' => 'Takk for at du forstår vår personvernerklæring.',
+    'agree' => 'Ved fortsatt bruk av dette systemet er du enig i vår personvernerklæring.',
     'privacy' => 'personvernerklæring',
-    'terms' => 'bruksvilkår',
     'understand' => 'Jeg forstår',
-    'updated' => 'Vi har oppdater våre',
+    'updated' => 'Vi har oppdatert vår',
 ];
