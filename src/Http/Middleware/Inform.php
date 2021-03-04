a24222e5405b94a07167a320df51616266829014
Lasse <1869424+lasseeee@users.noreply.github.com>
2021-03-04 08:07:54 +0100

    Remove terms of service

--- src/Http/Middleware/Inform.php
+++ src/Http/Middleware/Inform.php
@@ -14,19 +14,12 @@ class Inform
         if (auth()->user()->isNotInformed()) {
             flash()->info(
                 trans('info::messages.updated') .
-                ' <a href="' . route('info.terms') . '" style="color:#fff;text-decoration:underline!important;">' .
-                trans('info::messages.terms') .
-                '</a> ' .
-                trans('info::messages.and') .
                 ' <a href="' . route('info.privacy') . '" style="color:#fff;text-decoration:underline!important;">' .
                 trans('info::messages.privacy') .
                 '</a>. ' .
                 trans('info::messages.agree') .
                 ' <a href="' . route('info.understand') . '" class="btn btn-xs btn-primary margin-left">' .
                 trans('info::messages.understand') .
-                '</a>' .
-                ' <a href="' . route('info.faq') . '" class="btn btn-xs btn-default margin-left">' .
-                trans('info::messages.more') .
                 '</a>'
             )
             ->important();
