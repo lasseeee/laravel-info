5117466f08d71e4b5f995c2b5d2ec0676fc02869
Lasse <1869424+lasseeee@users.noreply.github.com>
2021-03-04 09:33:47 +0100

    Simplify language further

--- src/resources/lang/en/messages.php
+++ src/resources/lang/en/messages.php
@@ -1,9 +1,8 @@
 <?php
 
 return [
-    'agree' => 'By continued use of this system you agree to our privacy policy.',
     'privacy' => 'privacy policy',
     'understand' => 'I understand',
-    'understood' => 'Thank you for understanding our privacy policy.',
+    'understood' => 'Thank you for understanding our privacy policy',
     'updated' => 'We have updated our',
 ];
