88d318b02cdebe39b8488add927dec313d3a6d6d
Lasse <1869424+lasseeee@users.noreply.github.com>
2023-09-28 11:17:04 +0100

    Fix understood route guest bug

--- src/Http/Controllers/UnderstandController.php
+++ src/Http/Controllers/UnderstandController.php
@@ -2,11 +2,17 @@
 
 namespace Lasseeee\Info\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 class UnderstandController extends Controller
 {
     public function __invoke()
     {
-        auth()->user()->inform();
+        if (Auth::guest()) {
+            return redirect()->route('home');
+        }
+
+        Auth::user()->inform();
 
         flash()->success(trans('info::messages.understood'));
 
