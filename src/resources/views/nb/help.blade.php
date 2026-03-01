db18228bddc86757f2b92a824d54dfb0eaabcd6c
Lasse <1869424+lasseeee@users.noreply.github.com>
2026-03-01 22:05:17 +0100

    Make help link open in new tab

--- src/resources/views/nb/help.blade.php
+++ src/resources/views/nb/help.blade.php
@@ -1,7 +1,7 @@
 @component('info::help')
 ## Trenger du hjelp med å logge inn?
 
-Frivilligservice har en hjelpeside [her](https://www.mittrodekors.no/kontaktoss/) med svar og veiledning for de vanligste innloggingsutfordringene, samt kontaktinformasjon om du likevel står fast.
+Frivilligservice har en hjelpeside <a href="https://www.mittrodekors.no/kontaktoss/" target="_blank">her</a> med svar og veiledning for de vanligste innloggingsutfordringene, samt kontaktinformasjon om du likevel står fast.
 
 ## Trenger du hjelp med noe annet?
 
