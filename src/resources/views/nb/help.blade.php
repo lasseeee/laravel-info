009339f60f9d9510e9f898d700e10e1e7d25cb01
Lasse <1869424+lasseeee@users.noreply.github.com>
2026-03-01 21:06:45 +0100

    Expand Norwegian help page with login info

--- src/resources/views/nb/help.blade.php
+++ src/resources/views/nb/help.blade.php
@@ -1,5 +1,9 @@
 @component('info::help')
-## Trenger du hjelp?
+## Trenger du hjelp med å logge inn?
+
+Frivilligservice har en hjelpeside [her](https://www.mittrodekors.no/kontaktoss/) med svar og veiledning for de vanligste innloggingsutfordringene, samt kontaktinformasjon om du likevel står fast.
+
+## Trenger du hjelp med noe annet?
 
 Ta kontakt med en ansatt i din aktivitet.
 
